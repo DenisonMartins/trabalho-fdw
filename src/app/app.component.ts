77102686a556ff8b2dda7c6ffc82a57f14001e17
Michael Martins <michael.dl.mrtns@gmail.com>
2021-10-24 09:51:41 -0300

    Listagem de alunos do trablho de FDW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Aluno} from "./model/aluno";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabalho';
+  alunos: Array<Aluno>;
+
+  constructor() {
+    this.alunos = [
+      {
+        name: 'Michael Martins', ru: '3288341', curso: 'Analise e Desenvolvimento', dataNascimento: '22/08/1988'
+      },
+      {
+        name: 'Raphael Martins', ru: '1730713', curso: 'Educação Física', dataNascimento: '21/09/1993'
+      },
+      {
+        name: 'Ana Laura', ru: '0051548', curso: 'Enfermage', dataNascimento: '11/08/1970'
+      },
+      {
+        name: 'Valéria Martins', ru: '0149119', curso: 'Educação Física', dataNascimento: '28/04/1990'
+      },
+      {
+        name: 'João Alberto', ru: '7099197', curso: 'Medicina', dataNascimento: '15/04/1988'
+      }
+    ]
+  }
 }
---
+++ src/app/model/aluno.ts
@@ -0,0 +1,6 @@
+export interface Aluno {
+  name: string
+  ru: string
+  curso: string
+  dataNascimento: string
+}
